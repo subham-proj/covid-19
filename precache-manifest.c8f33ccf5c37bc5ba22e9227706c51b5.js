b3648a749510ca447c129b069fde9a20dbfdb4e8
subham-proj <3133ss32@gmail.com>
2020-05-16 09:16:01 +0530

    Updates

---
+++ precache-manifest.c8f33ccf5c37bc5ba22e9227706c51b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d299fe92c146ff6cfdda9c0a04d1c080",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "85c9035e1a10ae210889",
+    "url": "/covid-19/static/css/2.a1f65902.chunk.css"
+  },
+  {
+    "revision": "1114ec00fa29473f5e0b",
+    "url": "/covid-19/static/css/main.3961fd0d.chunk.css"
+  },
+  {
+    "revision": "85c9035e1a10ae210889",
+    "url": "/covid-19/static/js/2.57f9ffd0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/covid-19/static/js/2.57f9ffd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1114ec00fa29473f5e0b",
+    "url": "/covid-19/static/js/main.66b70752.chunk.js"
+  },
+  {
+    "revision": "c59e2f922549c307064d",
+    "url": "/covid-19/static/js/runtime-main.aabceb11.js"
+  }
+]);
